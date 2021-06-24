4edcc7e100ecbcbe5d999f503f4393966cb93f45
Aszhelt <aszhelt@gmail.com>
2021-06-24 16:59:10 +0300

    login log out register

--- MH_Training/core/views.py
+++ MH_Training/core/views.py
@@ -1,6 +1,18 @@
 from django.http import response
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
+from .forms import RegisterForm
 
 def home(response):
     return render(response, "core/home.html", {})
+
+def register(response):
+    if response.method == 'POST':
+        form = RegisterForm(response.POST)
+        if form.is_valid():
+            form.save()
+
+        return redirect('/')
+    else:
+        form = RegisterForm()
+    return render(response, 'register/register.html', {'form':form})
