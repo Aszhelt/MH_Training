77a5c3af6b17451c262a79895de74d99fe7b1c74
Aszhelt <aszhelt@gmail.com>
2021-07-12 14:20:36 +0300

    initial models according to MHT.png

--- MH_Training/storage/admin.py
+++ MH_Training/storage/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import ItemType, ItemGroup, Item, Storage, Container
+
+from .models import \
+    ItemType, ItemGroup, Item, \
+    Storage, ItemContainer, ItemRequest, \
+    EventType, Event
 
 
 class ItemTypeAdmin(admin.ModelAdmin):
@@ -18,12 +22,27 @@ class StorageAdmin(admin.ModelAdmin):
     list_display = ('name_storage',)
 
 
-class ContainerAdmin(admin.ModelAdmin):
-    list_display = ('item', 'storage', 'amount')
+class ItemContainerAdmin(admin.ModelAdmin):
+    list_display = ('item_container', 'storage_container', 'amount_container')
+
+
+class EventTypeAdmin(admin.ModelAdmin):
+    list_display = ('name_eventtype',)
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name_event', 'type_event', 'date_event')
+
+
+class ItemRequestAdmin(admin.ModelAdmin):
+    list_display = ('item_request', 'amount_itemrequest', 'storage_out', 'storage_in', 'status')
 
 
 admin.site.register(ItemType, ItemTypeAdmin)
 admin.site.register(ItemGroup, ItemGroupAdmin)
 admin.site.register(Item, ItemAdmin)
 admin.site.register(Storage, StorageAdmin)
-admin.site.register(Container, ContainerAdmin)
+admin.site.register(ItemContainer, ItemContainerAdmin)
+admin.site.register(EventType, EventTypeAdmin)
+admin.site.register(Event, EventAdmin)
+admin.site.register(ItemRequest, ItemRequestAdmin)
--- MH_Training/storage/models.py
+++ MH_Training/storage/models.py
@@ -25,7 +25,7 @@ class Item(models.Model):
 
     name_item = models.CharField(max_length=200)
     image_item = models.ImageField(upload_to='storage/item/')
-    type_item = models.ForeignKey(ItemType, related_name='type', on_delete=models.CASCADE)
+    type_item = models.ForeignKey(ItemType, related_name='type_item', on_delete=models.CASCADE)
     group_item = models.ManyToManyField(ItemGroup, related_name='group')
 
     def __str__(self):
@@ -40,11 +40,49 @@ class Storage(models.Model):
         return self.name_storage
 
 
-class Container(models.Model):
+class ItemContainer(models.Model):
 
-    item = models.ForeignKey(Item, related_name='item', on_delete=models.CASCADE)
-    amount = models.IntegerField()
-    storage = models.ForeignKey(Storage, related_name='storage', on_delete=models.CASCADE)
+    item_container = models.ForeignKey(Item, related_name='item_container', on_delete=models.CASCADE)
+    amount_container = models.IntegerField()
+    storage_container = models.ForeignKey(Storage, related_name='storage_container', on_delete=models.CASCADE)
 
     def __str__(self):
-        return str(self.item) + ' on ' + str(self.storage)
+        return str(self.item_container) + ' on ' + str(self.storage_container)
+
+
+class EventType(models.Model):
+
+    name_eventtype = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name_eventtype
+
+
+class Event(models.Model):
+
+    name_event = models.CharField(max_length=200)
+    date_event = models.DateField()
+    type_event = models.ForeignKey(EventType, related_name='type_event', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name_event
+
+
+class ItemRequest(models.Model):
+    STATUS_VARS = (
+        ('TD', 'ToDo'),
+        ('IP', 'InProgress'),
+        ('D', 'Done'),
+        ('UD', 'UnDone')
+    )
+
+    item_request = models.ForeignKey(Item, related_name='item_request', on_delete=models.CASCADE)
+    amount_itemrequest = models.IntegerField()
+    event_itemrequest = models.ForeignKey(Event, related_name='event_itemrequest', on_delete=models.CASCADE)
+    storage_out = models.ForeignKey(Storage, related_name='storage_out', on_delete=models.CASCADE)
+    storage_in = models.ForeignKey(Storage, related_name='storage_in', on_delete=models.CASCADE)
+    status = models.CharField(max_length=2, choices=STATUS_VARS)
+
+    def __str__(self):
+        return str(self.amount_itemrequest) + '(' + str(self.item_request) + ') ' \
+               + str(self.storage_out) + ' to ' + str(self.storage_in)
