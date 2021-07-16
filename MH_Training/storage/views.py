740ce40abefc9f0ed9680671479e8faffaa9b9bc
Aszhelt <aszhelt@gmail.com>
2021-07-16 23:30:35 +0300

    Orders view, in 3 group

--- MH_Training/storage/views.py
+++ MH_Training/storage/views.py
@@ -9,9 +9,27 @@ def orders(response):
     if not response.user.is_authenticated:
         return HttpResponseRedirect('/login')
     else:
-        user_orders = Order.objects.filter(user_order=response.user). \
-            filter(Q(status_order='D') | Q(status_order='TD')
-                   | Q(status_order='IP') | Q(status_order='IR'))  # get not closed orders created by user
+        user_storages = Storage.objects.filter(user_storage=response.user)
+        is_multi_storage = user_storages.count() > 1
+        not_closed_orders = Order.objects.filter(Q(status_order='D') | Q(status_order='TD')
+                                                 | Q(status_order='IP') | Q(status_order='IR'))
+
+        # get not closed orders created by user
+        user_orders = not_closed_orders.filter(user_order=response.user)
+
+        # get not closed orders from user`s storage
+        orders_from_user_storage = not_closed_orders.filter(storage_from__in=user_storages)
+
+        # get other orders but not closed
+        other_orders = not_closed_orders.exclude(storage_from__in=user_storages).\
+            exclude(user_order=response.user)
+
+        order_groups = (('My orders', user_orders),
+                        ('Orders from my storage', orders_from_user_storage),
+                        ('Other orders', other_orders))
 
         return render(response, 'storage/orders.html',
-                      {'user_orders': user_orders})
+                      {
+                          'is_multi_storage': is_multi_storage,
+                          'order_groups': order_groups,
+                      })
